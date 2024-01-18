3dc97895a2764543f2d2ed2123332b266d061cb6
maderami <sdsoerty6584@gmail.com>
2024-01-18 23:15:45 +0600

    even commit

--- index.php
+++ index.php
@@ -45,36 +45,56 @@ $example_persons_array = [
         'job' => 'babysitter',
     ],
 ];
+function getPartsFromFullname($snl) {
+    $Name = explode(' ', $snl);
+    $SNL = [
+        'last_name' => $Name[0],
+        'name' => $Name[1],
+        'second_name' => $Name[2],
+    ];
+    return $SNL;
+};
+function getFullnameFromParts($surname, $name, $patronomyc){
+    $fullname =  $surname ." " .$name ." " .$patronomyc;
+    return $fullname;
+};
+function getShortName($person){
+    $seporated = getPartsFromFullname($person);
+    $shortName = $seporated["name"] . ' ' . mb_substr($seporated["last_name"],0,1). ".";
+    return $shortName;
+};
+function getGenderFromName($fullName){
+    $fullN = getPartsFromFullname($fullName);
+    $genderPerson = 0;
 
-function getShortName($array){
+    if((mb_substr($fullN['last_name'], -2,2) == 'ая' || mb_substr($fullN['last_name'], -1,1) == 'я' ) || (mb_substr($fullN['name'], -1,1) == 'а' || mb_substr($fullN['name'], -1,1) == 'я' || mb_substr($fullN['name'], -1,1) == 'ь' ) || mb_substr($fullN['second_name'], -2,2) == 'на'){
+        $genderPerson = -1;
+    }elseif((mb_substr($fullN['last_name'], -1,1) == 'н' || mb_substr($fullN['last_name'], -1,1) == 'к' || mb_substr($fullN['last_name'], -2,2) == 'ой'  || mb_substr($fullN['last_name'], -2,2) == 'ов' || mb_substr($fullN['last_name'], -1,1) == 'о' ) || (mb_substr($fullN['name'], -1,1) == 'а' || mb_substr($fullN['name'], -1,1) == 'я' || mb_substr($fullN['name'], -1,1) == 'ь' ) || mb_substr($fullN['second_name'], -2,2) == 'ич') {
+        $genderPerson = 1;
+    }else{
+        $genderPerson = 0;
+    }
 
-    return explode($array);
+    if($genderPerson === -1){
+        return 'Женщина';
+    }elseif ($genderPerson === 1){
+        return 'Мужчина';
+    }else{
+        return 'Не возможно определить пол';
+    }
 
 }
 
-function getPartsFromFullname(){
-
-}
-
-function getFullnameFromParts(){
-
-}
-
-function getGenderFromName(){
-
-}
-
-function getGenderDescription() {
-
-}
-
-function getPerfectPartner(){
-
-}
-
+function getGenderDescription($arraygender){}
+function getPerfectPartner($lastname, $name, $secondname, $arrayExample){}
 ?>
 
 
+
 <div>
+    <p><?=print_r(getPartsFromFullname('Степанова Наталья Степановна'));?></p>
+    <p><?=getFullnameFromParts('Степанова', 'Наталья', 'Степановна');?></p>
+    <p><?=getShortName('Степанова Наталья Степановна');?></p>
+    <p><?=getGenderFromName('Пащенко Владимир Александрович');?></p>
 
 </div>
